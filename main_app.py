0718c29021db85156a6156618c707d8ad08770e8
YCY5449 <469779590@qq.com>
2024-12-12 17:06:36 +0800

    update t_home

--- main_app.py
+++ main_app.py
@@ -4,37 +4,37 @@ from flask import Flask,render_template
 app = Flask(__name__)
 @app.route('/')
 def main():
-    return render_template("HomeTemplate.html")
+    return render_template("HomeTemplate.html", t_home='/#games')
 @app.route('/PinballGame')
 def PinballGame():
-    return render_template("PinballGame.html")
+    return render_template("PinballGame.html", t_home='/#games')
 @app.route('/PlaneWarsGame')
 def FlyGame():
-    return render_template("PlaneWarsGame.html")
+    return render_template("PlaneWarsGame.html", t_home='/#games')
 @app.route('/GreedySnakeGame')
 def GreedySnakeGame():
-    return render_template("GreedySnakeGame.html")
+    return render_template("GreedySnakeGame.html", t_home='/#games')
 @app.route('/FruitNinjaGame')
 def FruitNinjaGame():
-    return render_template("FruitNinjaGame.html")
+    return render_template("FruitNinjaGame.html", t_home='/#games')
 @app.route('/MarioGame')
 def MarioGame():
-    return render_template("MarioGame.html")
+    return render_template("MarioGame.html", t_home='/#games')
 @app.route('/2048Game')
 def TwentyFortyEightGame():
-    return render_template("TwentyFortyEightGame.html")
+    return render_template("TwentyFortyEightGame.html", t_home='/#games')
 @app.route('/TetrisGame')
 def TetrisGame():
-    return render_template("TetrisGame.html")
+    return render_template("TetrisGame.html", t_home='/#games')
 @app.route('/TwoPlayersTetrisGame')
 def TwoPlayersTetrisGame():
-    return render_template("TwoPlayersTetrisGame.html")
+    return render_template("TwoPlayersTetrisGame.html", t_home='/#games')
 @app.route('/T-RexDinoGame')
 def T_RexDinoGame():
-    return render_template("T-RexDinoGame.html")
+    return render_template("T-RexDinoGame.html", t_home='/#games')
 @app.route('/MinesweeperGame')
 def MinesweeperGame():
-    return render_template("MinesweeperGame.html")
+    return render_template("MinesweeperGame.html", t_home='/#games')
 
 
 if __name__ == '__main__':
